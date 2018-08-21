e1e28a84fa2c93e7d0ce1565b3dceed3de46fc3c
Jansel Rodriguez <jvra16@gmail.com>
2018-08-21 19:08:36 -0400

    First commit

---
+++ EasingFunctions.swift
@@ -0,0 +1,15 @@
+//
+//  EasingFunctions.swift
+//  Easing
+//
+//  Created by Jansel Valentin on 5/13/16.
+//  Copyright © 2016 Jansel Valentin. All rights reserved.
+//
+
+import Foundation
+
+
+typealias Easing        = (_ t:CGFloat,_ b:CGFloat,_ c:CGFloat,_ d:CGFloat)-> CGFloat
+typealias BackEasing    = (_ t:CGFloat,_ b:CGFloat,_ c:CGFloat,_ d:CGFloat,_ s:CGFloat)-> CGFloat
+typealias ElasticEasing = (_ t:CGFloat,_ b:CGFloat,_ c:CGFloat,_ d:CGFloat,_ a:CGFloat,_ p:CGFloat)-> CGFloat
+
